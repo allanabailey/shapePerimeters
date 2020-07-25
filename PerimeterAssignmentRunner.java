dd89442cf75cae210ead5326a30db876622a08bd
Allana Bailey <47748433+allanabailey@users.noreply.github.com>
2020-07-25 20:44:14 +0100

    updated perimeterrunner with comments

--- PerimeterAssignmentRunner.java
+++ PerimeterAssignmentRunner.java
@@ -1,27 +1,23 @@
 package perimeter_quiz;
-
+//"https://www.dukelearntoprogram.com//course2/files.php"
 import edu.duke.*;
+//"https://www.dukelearntoprogram.com/course1/doc/"
 import java.io.File;
 
+
 public class PerimeterAssignmentRunner {
+    //calculate the total perimeter of a shape, starting with the 'last' point.
     public double getPerimeter (Shape s) {
-        // Start with totalPerim = 0
         double totalPerim = 0.0;
-        // Start wth prevPt = the last point 
         Point prevPt = s.getLastPoint();
-        // For each point currPt in the shape,
         for (Point currPt : s.getPoints()) {
-            // Find distance from prevPt point to currPt 
             double currDist = prevPt.distance(currPt);
-            // Update totalPerim by currDist
             totalPerim = totalPerim + currDist;
-            // Update prevPt to be currPt
             prevPt = currPt;
         }
-        // totalPerim is the answer
         return totalPerim;
     }
-
+    //calculate the number of distinct individual points in any given shape.
     public int getNumPoints(Shape s) {
         int numPoints = 0;
         for(Point countPt : s.getPoints()) {
@@ -29,7 +25,7 @@ public class PerimeterAssignmentRunner {
         }
         return numPoints;
     }
-
+    //Calculate the average length of all the sides of a given shape.
     public double getAverageLength(Shape s) {
         double avgLength = 0.0;
         double length = getPerimeter(s);
@@ -37,7 +33,7 @@ public class PerimeterAssignmentRunner {
         avgLength = length / numSides;
         return avgLength;
     }
-
+    //Identify the length of the longest side of a given shape.
     public double getLargestSide(Shape s) {
         double longestSide = 0.0;
         Point prevPt = s.getLastPoint();
@@ -54,7 +50,7 @@ public class PerimeterAssignmentRunner {
         }  
         return longestSide;
     }
-
+    //find the largest x value of any pixel from a given shape.
     public double getLargestX(Shape s) {
         double biggestX = 0.0;
         for(Point currPt : s.getPoints()) {
@@ -65,7 +61,7 @@ public class PerimeterAssignmentRunner {
         }
         return biggestX;
     }
-
+    //return the largest total perimeter from multiple files containing multiple shapes.
     public double getLargestPerimeterMultipleFiles() {
         DirectoryResource dr = new DirectoryResource();
         double longestPerim = 0.0;
@@ -79,7 +75,7 @@ public class PerimeterAssignmentRunner {
         }
         return longestPerim;
     }
-
+    //return the filename containing the shape with the largest perimeter.
     public File getFileWithLargestPerimeter() {
         DirectoryResource dr = new DirectoryResource();
         String filename = null;
@@ -98,7 +94,7 @@ public class PerimeterAssignmentRunner {
         }
         return longFile; 
     }
-
+    //test all methods above.
     public void testPerimeter () {
         FileResource fr = new FileResource();
         Shape s = new Shape(fr);
@@ -113,18 +109,17 @@ public class PerimeterAssignmentRunner {
         double largestX = getLargestX(s);
         System.out.println("Largest x value = " + largestX);
     }
-    
+    //test if the correct total perimeter is returned.
     public void testPerimeterMultipleFiles() {
         double largestPerim = getLargestPerimeterMultipleFiles();
         System.out.println("Largest perimeter = " + largestPerim);
     }
-
+    //test if the file returned does contain the shape with the largest perimeter.
     public void testFileWithLargestPerimeter() {
         File largestPerim = getFileWithLargestPerimeter();
         System.out.println("File with largest perimeter is " + largestPerim);
     }
-
-    // This method creates a triangle that you can use to test your other methods
+    //Creates a triangle that can be used to test other methods.
     public void triangle(){
         Shape triangle = new Shape();
         triangle.addPoint(new Point(0,0));
@@ -134,17 +129,15 @@ public class PerimeterAssignmentRunner {
             System.out.println(p);
         }
         double peri = getPerimeter(triangle);
-        System.out.println("perimeter = "+peri);
+        System.out.println("perimeter = " + peri);
     }
-
-    // This method prints names of all files in a chosen folder that you can use to test your other methods
+    //Prints all of the names of files within a given folder.
     public void printFileNames() {
         DirectoryResource dr = new DirectoryResource();
         for (File f : dr.selectedFiles()) {
             System.out.println(f);
         }
     }
-
     public static void main (String[] args) {
         PerimeterAssignmentRunner pr = new PerimeterAssignmentRunner();
         pr.testPerimeter();
